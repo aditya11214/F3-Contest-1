00c1a70f98717894663be99cc74a95cc7332a6d8
Aditya Rajput <aditya.thehuman@gmail.com>
2023-09-16 19:31:21 +0530

    IMDB Website

---
+++ script.js
@@ -0,0 +1,109 @@
+const searchbtn = document.getElementById("btn")
+const apikey= document.getElementById("apikey")
+const search= document.getElementById("search")
+const cardsDiv = document.getElementById("M-cards")
+const loader = document.getElementById("loader");
+const msg = document.getElementById("errorMsg")
+const moreDetails= document.getElementById("moreDetails")
+const details=document.getElementById("details")
+
+ searchbtn.addEventListener("click",(e)=>{
+   e.preventDefault();
+   loader.style.display="block";
+   msg.style.display="none";
+   cardsDiv.innerHTML="";
+   let key = apikey.value;
+   let searchValue=search.value;
+   if(!key || !searchValue)
+   {
+       msg.style.display="block";
+       msg.innerHTML="Please provide api_key and Movie title!"
+       loader.style.display="none";
+
+   }
+   else{
+       fethDetails(key,searchValue)
+   }
+ })
+
+async function fethDetails(key ,searchValue){
+   try{
+       const response = await fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=${key}`);
+         const data = await response.json();
+         if(data.Error){
+           loader.style="display:none";
+           details.innerHTML="";
+           msg.style.display="block";
+           msg.innerHTML=`${data.Error}`
+         }
+         else{
+           RenderData(data.Search);
+         }
+   }
+   catch(error){
+       loader.style="display:none";
+       details.innerHTML="";
+       console.error(error);
+   }
+
+}
+function RenderData(data){
+   cardsDiv.innerHTML="";
+   details.innerHTML="";
+    let count=1;
+    msg.style.display="none";
+    loader.style="display:none";
+   data.forEach((item)=>{
+       let card = document.createElement("div")
+       card.className="card";
+       card.innerHTML=` <img src="${item.Poster}" alt="N/A">
+       <div>
+        <p class="num">${count}</p>
+        <div class="details">
+            <p>${item.Title}</p>
+           
+            <p id="moreDetails">More Details ><p>
+        </div>`
+        count++;
+        cardsDiv.appendChild(card);
+
+       card.addEventListener("click",()=>renderDetails(item.imdbID));
+
+   });
+     
+}
+
+
+async function renderDetails(Id){
+   loader.style="display:block";
+   cardsDiv.innerHTML="";
+   details.innerHTML="";
+   const response= await fetch(`https://www.omdbapi.com/?i=${Id}&apikey=${apikey.value}`)
+   const data = await response.json();
+         if(data.Error){
+           loader.style="display:none";
+           msg.style.display="block";
+           msg.innerHTML=`${data.Error}`
+         }
+         else{
+           loader.style="display:none";
+          const cardDe= document.createElement("div")
+          cardDe.className="detailsCard";
+          cardDe.innerHTML=`<img src="${data.Poster}" alt="poster">
+          <div>
+             <p>Title : ${data.Title}</p>
+             <p>Type : ${data.Type}</p>
+             <p>Release Date : ${data.Released}</p>
+             <p>Genre : ${data.Genre}</p>
+             <p>Director : ${data.Director}</p>
+             <p>Actors : ${data.Actors} </p>
+             <p>imdb Rating : ${data.imdbRating}</p>
+             <p>imdb Votes :${data.imdbVotes}</p>
+          </div>`
+           details.appendChild(cardDe);
+
+         }
+
+}
+
+
